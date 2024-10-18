1349835e54e325a2a2fa158abdae33682c143e52
BigTodd <lolusun2000@gmail.com>
2024-10-18 11:06:50 +0200

    Add files via upload

---
+++ jo.py
@@ -0,0 +1,76 @@
+
+# zadanie 1
+# nb = [-2, 3, 4, -1, 2, 0, 3, -7, 2, 5]
+
+# for br in nb:
+#     if br >= 0:
+#         print(br)
+
+# zadanie 2
+
+# nb = [-2, 3, 4, -1, 2, 0, 3, -7, 2, 5]
+# dlu = len(nb)
+# ind = 0
+
+# while ind < dlu:
+#     if nb[ind] % 2 == 0:
+#         print(nb[ind])
+#     ind += 1    
+    
+# nb = [-2, 3, 4, -1, 2, 0, 3, -7, 2, 5]
+# non = len(nb)
+# cun = 0
+
+# while cun < non:
+#     if cun % 3 == 0:
+#         print(nb[cun])
+#     cun += 1
+
+# class Automobile:
+#     def __init__(self, marka, model, kolor):
+#         self.marka = marka
+#         self.model = model
+#         self.kolor = kolor
+
+#     def sluz (self):
+#         print(self.marka, self.model, self.kolor)
+
+# auto = Automobile('nissan', 'almera', 'błękitny')
+# auto.sluz()
+
+#zadanie 5
+
+# class Squirtle:
+#     def __init__(self, side):
+#         self.side = side
+
+#     def brzu (self):
+#         print("area: ", self.side ** 2)
+
+#     def bab (self): 
+#         print("SUM side length: ", self.side * 4)
+    
+# ji = Squirtle(int(input("Side length: ")))
+# ji.brzu()
+# ji.bab()
+
+
+import tkinter
+
+# root = tkinter.Tk()
+
+# root.title("Title")
+# root.geometry("300x300")
+
+# frame = tkinter.Frame(root)
+
+# label = tkinter.Label(frame, text = "Text").grid(column = 0, row = 0)
+# butt = tkinter.Button(frame, text = "Button").grid(column = 1, row = 0)
+
+# frame.pack(anchor = "center", pady = "120")
+
+
+# root.mainloop()
+
+
+
